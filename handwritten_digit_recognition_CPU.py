9a85794f234da967cac2e4b102dd7248577e8f4a
Renyuan Lyu <renyuan.lyu@gmail.com>
2021-05-16 18:25:44 +0800

    adding Chinese Comments

--- handwritten_digit_recognition_CPU.py
+++ handwritten_digit_recognition_CPU.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# <td>
+# <a target="_blank" href="https://colab.research.google.com/github/renyuanL/handwritten-digit-recognition/blob/master/handwritten_digit_recognition_CPU.ipynb"><img src="https://www.tensorflow.org/images/colab_logo_32px.png" />Run this in Google Colab</a>
+# </td>
+#  
+
+# - https://colab.research.google.com/github/renyuanL/handwritten-digit-recognition/blob/master/handwritten_digit_recognition_CPU.ipynb
+# 
+# 
+
 # # Handwritten Digit Recognition
-# - Author = Amitrajit Bose
-# - Dataset = MNIST
+# ## 手寫數字辨識
+# 
+# - Authors= 
+#     - Amitrajit Bose, 
+#     - Renyuan Lyu from CGU, Taiwan
+# - Dataset= MNIST
 # - [Medium Article Link](https://medium.com/@amitrajit_bose/handwritten-digit-mnist-pytorch-977b5338e627)
 # - Frameworks = PyTorch
 # 
 
 # ### Necessary Imports
 
-# In[ ]:
+# In[1]:
 
 
 # Import necessary packages
+# 引入(import) 必要的模組 (modules)
+
 get_ipython().run_line_magic('matplotlib', 'inline')
 get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'retina'")
 
@@ -24,11 +39,11 @@ import matplotlib.pyplot as plt
 from time import time
 
 
-# In[ ]:
+# In[2]:
 
 
 import os
-from google.colab import drive
+#from google.colab import drive
 
 
 # ### Download The Dataset & Define The Transforms
@@ -36,7 +51,8 @@ from google.colab import drive
 # In[3]:
 
 
-### Run this cell
+# 蒐集、下載、整理 手寫數字資料 (MNIST)，以供實驗
+# 第一次執行較花時間，第2次以後可關閉下載功能，以節省時間。
 
 from torchvision import datasets, transforms
 
@@ -46,10 +62,20 @@ transform = transforms.Compose([transforms.ToTensor(),
                               ])
 
 # Download and load the training data
-trainset = datasets.MNIST('drive/My Drive/mnist/MNIST_data/', download=True, train=True, transform=transform)
-valset = datasets.MNIST('drive/My Drive/mnist/MNIST_data/', download=True, train=False, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
-valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+trainset= datasets.MNIST('drive/My Drive/mnist/MNIST_data/', 
+                         download= True,  # 第一次執行須 True, 以後就可改為False
+                         train=True, 
+                         transform=transform)
+valset=   datasets.MNIST('drive/My Drive/mnist/MNIST_data/', 
+                         download= True, 
+                         train=False, transform=transform)
+
+trainloader= torch.utils.data.DataLoader(trainset, 
+                                         batch_size=64, 
+                                         shuffle=True)
+valloader=   torch.utils.data.DataLoader(valset,   
+                                         batch_size=64, 
+                                         shuffle=True)
 
 
 # ### Exploring The Data
@@ -57,7 +83,9 @@ valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
 # In[4]:
 
 
-dataiter = iter(trainloader)
+# 觀賞一下資料庫
+
+dataiter= iter(trainloader)
 images, labels = dataiter.next()
 print(type(images))
 print(images.shape)
@@ -67,7 +95,8 @@ print(labels.shape)
 # In[5]:
 
 
-plt.imshow(images[0].numpy().squeeze(), cmap='gray_r');
+x= images[0].numpy().squeeze()
+plt.imshow(x, cmap='gray_r');
 
 
 # In[6]:
@@ -78,7 +107,8 @@ num_of_images = 60
 for index in range(1, num_of_images + 1):
     plt.subplot(6, 10, index)
     plt.axis('off')
-    plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
+    x= images[index].numpy().squeeze()
+    plt.imshow(x, cmap='gray_r')
 
 
 # ### Defining The Neural Network
@@ -87,6 +117,8 @@ for index in range(1, num_of_images + 1):
 
 # In[7]:
 
+
+# 定義 神經網路的架構參數
 
 from torch import nn
 
@@ -105,95 +137,7 @@ model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
 print(model)
 
 
-# In[ ]:
-
-
-criterion = nn.NLLLoss()
-images, labels = next(iter(trainloader))
-images = images.view(images.shape[0], -1)
-
-logps = model(images)
-loss = criterion(logps, labels)
-
-
-# In[9]:
-
-
-print('Before backward pass: \n', model[0].weight.grad)
-
-loss.backward()
-
-print('After backward pass: \n', model[0].weight.grad)
-
-
-# In[ ]:
-
-
-from torch import optim
-
-# Optimizers require the parameters to optimize and a learning rate
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
-
-
-# In[11]:
-
-
-print('Initial weights - ', model[0].weight)
-
-images, labels = next(iter(trainloader))
-images.resize_(64, 784)
-
-# Clear the gradients, do this because gradients are accumulated
-optimizer.zero_grad()
-
-# Forward pass, then backward pass, then update weights
-output = model(images)
-loss = criterion(output, labels)
-loss.backward()
-print('Gradient -', model[0].weight.grad)
-
-
-# In[12]:
-
-
-# Take an update step and few the new weights
-optimizer.step()
-print('Updated weights - ', model[0].weight)
-
-
-# ### Core Training Of Neural Network
-
-# In[13]:
-
-
-optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
-time0 = time()
-epochs = 15
-for e in range(epochs):
-    running_loss = 0
-    for images, labels in trainloader:
-        # Flatten MNIST images into a 784 long vector
-        images = images.view(images.shape[0], -1)
-    
-        # Training pass
-        optimizer.zero_grad()
-        
-        output = model(images)
-        loss = criterion(output, labels)
-        
-        #This is where the model learns by backpropagating
-        loss.backward()
-        
-        #And optimizes its weights here
-        optimizer.step()
-        
-        running_loss += loss.item()
-    else:
-        print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainloader)))
-print("\nTraining Time (in minutes) =",(time()-time0)/60)
-
-
-# In[ ]:
+# In[8]:
 
 
 def view_classify(img, ps):
@@ -213,8 +157,10 @@ def view_classify(img, ps):
     plt.tight_layout()
 
 
-# In[15]:
+# In[9]:
 
+
+# 在訓練之前，先測試一下辨識效能：
 
 images, labels = next(iter(valloader))
 
@@ -230,10 +176,10 @@ print("Predicted Digit =", probab.index(max(probab)))
 view_classify(img.view(1, 28, 28), ps)
 
 
-# ### Model Evaluation
+# In[10]:
 
-# In[16]:
 
+# 全面測試辨識效能
 
 correct_count, all_count = 0, 0
 for images,labels in valloader:
@@ -254,4 +200,177 @@ for images,labels in valloader:
 
 print("Number Of Images Tested =", all_count)
 print("\nModel Accuracy =", (correct_count/all_count))
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+# 開始準備訓練
+
+criterion = nn.NLLLoss()
+images, labels = next(iter(trainloader))
+images = images.view(images.shape[0], -1)
+
+logps = model(images)
+loss = criterion(logps, labels)
+
+
+# In[12]:
+
+
+loss, logps 
+
+
+# In[13]:
+
+
+print(f'Before backward pass: weight= {model[0].weight},\n weightGrad= {model[0].weight.grad}\n')
+
+
+# In[14]:
+
+
+loss.backward()
+
+# 執行 .backward() 之後， weight 本身不變，但 weightGrad 被更新了！
+print(f'Before backward pass: weight= {model[0].weight},\n weightGrad= {model[0].weight.grad}\n')
+
+
+# In[15]:
+
+
+# 指定 Optimization 的演算法為 SGD (Stochastic Gradient Decent)
+
+from torch import optim
+
+# Optimizers require the parameters to optimize and a learning rate
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+# In[16]:
+
+
+print('Initial weights - ', model[0].weight)
+
+images, labels = next(iter(trainloader))
+images.resize_(64, 784)
+
+# Clear the gradients, do this because gradients are accumulated
+optimizer.zero_grad()
+
+# Forward pass, then backward pass, then update weights
+output = model(images)
+loss = criterion(output, labels)
+loss.backward()
+print('Gradient -', model[0].weight.grad)
+
+
+# In[17]:
+
+
+# Take an update step and few the new weights
+optimizer.step()
+print('Updated weights - ', model[0].weight)
+
+
+# ### Core Training Of Neural Network
+
+# In[ ]:
+
+
+optimizer = optim.SGD(model.parameters(), 
+                      lr= 0.003, 
+                      momentum= 0.9)
+
+time0 = time() # 監控一下時間，記錄當前時間
+
+epochs= 15  # 這個 epochs 可改小，以免跑太久，但若要提升辨識率，則應該改大一點。
+
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        # Flatten MNIST images into a 784 long vectors
+        images = images.view(images.shape[0], -1)
+    
+        # Training pass
+        optimizer.zero_grad()
+        
+        output = model(images)
+        loss = criterion(output, labels)
+        
+        #This is where the model learns by backpropagating
+        loss.backward()
+        
+        #And optimizes its weights here
+        optimizer.step()
+        
+        running_loss += loss.item()
+    
+    print(f"Epoch {e} - Training loss: {running_loss/len(trainloader)}")
+    print(f"Acc Training Time (in minutes)= {(time()-time0)/60:.3f}\n")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# 訓練後，再測試一下辨識效能：
+
+images, labels = next(iter(valloader))
+
+img = images[0].view(1, 784)
+# Turn off gradients to speed up this part
+with torch.no_grad():
+    logps = model(img)
+
+# Output of the network are log-probabilities, need to take exponential for probabilities
+ps = torch.exp(logps)
+probab = list(ps.numpy()[0])
+print("Predicted Digit =", probab.index(max(probab)))
+view_classify(img.view(1, 28, 28), ps)
+
+
+# ### Model Evaluation
+
+# In[ ]:
+
+
+# 訓練後，全面測試辨識效能
+
+correct_count, all_count = 0, 0
+for images,labels in valloader:
+  for i in range(len(labels)):
+    img = images[i].view(1, 784)
+    # Turn off gradients to speed up this part
+    with torch.no_grad():
+        logps = model(img)
+
+    # Output of the network are log-probabilities, need to take exponential for probabilities
+    ps = torch.exp(logps)
+    probab = list(ps.numpy()[0])
+    pred_label = probab.index(max(probab))
+    true_label = labels.numpy()[i]
+    if(true_label == pred_label):
+      correct_count += 1
+    all_count += 1
+
+print("Number Of Images Tested =", all_count)
+print("\nModel Accuracy =", (correct_count/all_count))
+
+
+# In[ ]:
+
+
+
 
